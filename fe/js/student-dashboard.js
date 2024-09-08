27a03bc604adf90ba997bc6aa4051fe8f3ebd35d
gahbriehel <babatise002@gmail.com>
2024-09-08 02:12:41 +0100

    created student dashbpard with styling

---
+++ fe/js/student-dashboard.js
@@ -0,0 +1,4 @@
+let welcomeText = document.getElementById("welcome");
+let profileName = "Gabriel";
+profileName = profileName.toUpperCase();
+welcomeText.innerHTML = `WELCOME ${profileName}`;
--- fe/js/teacher-form.js
+++ fe/js/teacher-form.js
@@ -1,31 +1,30 @@
-document.getElementById("form-id").addEventListener("submit", function (e) {
-  e.preventDefault();
+document
+  .getElementById("form-id-teacher")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  const form = e.target;
-  const formData = new FormData(form); // Create FormData object from form
+    const form = e.target;
+    const formData = new FormData(form);
 
-  // Convert form data to JSON object
-  const data = Object.fromEntries(formData.entries());
+    const data = Object.fromEntries(formData.entries());
 
-  fetch("http://localhost:5000/api/teachers", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data), // Send JSON data
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
+    fetch("http://localhost:5000/api/teachers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .then((result) => {
-      console.log("Success:", result);
-      // Handle success (show message, redirect, etc.)
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      // Handle error (show error message, etc.)
-    });
-});
+      .then((response) => {
+        if (response.status == 200 || response.status == 201) {
+          window.location.href = "./";
+        }
+        return response.json();
+      })
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
