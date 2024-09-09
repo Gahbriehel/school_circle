fdb14fccad930decbb3f29e6acacdb9087c3a44a
gahbriehel <babatise002@gmail.com>
2024-09-09 10:35:06 +0100

    added functionality to redirect on student registration successful

--- fe/js/form.js
+++ fe/js/form.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.getElementById("form-id").addEventListener("submit", function (e) {
   e.preventDefault();
   const form = e.target;
-  const formData = new FormData(form); // Create FormData object from form
+  const formData = new FormData(form);
 
   // Convert form data to JSON object
   const data = Object.fromEntries(formData.entries());
@@ -26,13 +26,19 @@ document.getElementById("form-id").addEventListener("submit", function (e) {
     },
     body: JSON.stringify(data), // Send JSON data
   })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log("Success:", result);
-      // Handle success (show message, redirect, etc.)
+    .then((response) => {
+      if (response.status == 200 || response.status == 201) {
+        window.location.href = "./student-dashboard.html";
+      } else {
+        alert("Oops! There was a problem submitting the form");
+        return response.json().then((errorData) => {
+          console.error("Error:", errorData);
+        });
+      }
     })
     .catch((error) => {
       console.error("Error:", error);
       // Handle error (show error message, etc.)
+      alert("An unexpected error occurred. Please try again.");
     });
 });
