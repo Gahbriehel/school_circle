913720d0d36939b0daa35a3f0313e485f272e033
gahbriehel <babatise002@gmail.com>
2024-09-11 11:47:51 +0100

    made changes to signin flow

---
+++ fe/js/pre-signin.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const optionButtons = document.querySelectorAll(".selection-btn");
+  const continueButton = document.getElementById("continue-btnn");
+  let selectedOption = null;
+  let selectedTag;
+
+  optionButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      optionButtons.forEach((btn) => btn.classList.remove("selected"));
+      this.classList.add("selected");
+
+      selectedOption = this.id;
+      selectedTag = this.name;
+
+      continueButton.classList.add("enabled");
+      continueButton.disabled = false;
+    });
+  });
+
+  continueButton.addEventListener("click", function () {
+    if (selectedOption == "student-btnn") {
+      alert(`You are signing in as a ${selectedTag}`);
+      window.location.href = "./sign-in.html";
+    } else if (selectedOption == "teacher-btnn") {
+      alert(`You are signing in as a ${selectedTag}`);
+      window.location.href = "./sign-in.html";
+    } else {
+      alert(`You are signing in as an ${selectedTag}`);
+      window.location.href = "./sign-in.html";
+    }
+  });
+});
