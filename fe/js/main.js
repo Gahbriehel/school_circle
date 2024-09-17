48ff2ef66e9a1b4a9d8a84a6361f64c4c35c0315
gahbriehel <babatise002@gmail.com>
2024-09-17 03:24:07 +0100

    made homepage responsive for smaller screens

--- fe/js/main.js
+++ fe/js/main.js
@@ -1,11 +1,35 @@
 // alert("Welcome!!!")
-const nav = document.querySelector(".nav_links");
+const nav = document.querySelector(".nav-links");
 const signupBtn = document.getElementById("get-started-btn");
 
 window.addEventListener("scroll", () => {
   document
     .querySelector("nav")
     .classList.toggle("window-scrolled", window.scrollY > 0);
+});
+
+// Nav behaviour for responsiveness
+const openNavBtn = document.querySelector("#nav_toggle-open");
+const closeNavBtn = document.querySelector("#nav_toggle-close");
+
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+openNavBtn.addEventListener("click", openNav);
+
+const closeNav = () => {
+  nav.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+nav.querySelectorAll("li a").forEach((navLink) => {
+  navLink.addEventListener("click", closeNav);
 });
 
 //
@@ -61,27 +85,3 @@ const goSignup = () => {
 
 signupBtn.onclick = goSignup;
 tourBtn.onclick = goTour;
-
-// Nav behaviour for responsiveness
-const openNavBtn = document.querySelector("#nav_toggle-open");
-const closeNavBtn = document.querySelector("#nav_toggle-close");
-
-const openNav = () => {
-  nav.style.display = "flex";
-  openNavBtn.style.display = "none";
-  closeNavBtn.style.display = "inline-block";
-};
-
-openNavBtn.addEventListener("click", openNav);
-
-const closeNav = () => {
-  nav.style.display = "none";
-  openNavBtn.style.display = "inline-block";
-  closeNavBtn.style.display = "none";
-};
-
-closeNavBtn.addEventListener("click", closeNav);
-
-nav.querySelectorAll("li a").forEach((navLink) => {
-  navLink.addEventListener("click", closeNav);
-});
